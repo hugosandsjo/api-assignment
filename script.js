cfb3e29b7aec85b083b50763249ee2beef1e33c8
hugosandsjo <hugosandsjo@gmail.com>
2023-12-03 11:05:04 +0100

    fixed error catch and styling of button

--- script.js
+++ script.js
@@ -68,40 +68,49 @@
 // document.querySelector("#random-artwork").addEventListener("click", fetchData);
 
 const fetchData = async () => {
-  // Generate a random page number between 1 and 100
-  const page = Math.floor(Math.random() * 1000) + 1;
+  try {
+    // Generate a random page number between 1 and 100
+    const page = Math.floor(Math.random() * 1000) + 1;
 
-  const res = await fetch(`https://api.artic.edu/api/v1/artworks?page=${page}`);
-  const data = await res.json();
-  const iiifUrl = data.config.iiif_url;
+    const res = await fetch(
+      `https://api.artic.edu/api/v1/artworks?page=${page}`,
+    );
+    const data = await res.json();
+    const iiifUrl = data.config.iiif_url;
 
-  // Clear the body or another container element
-  document.querySelector("#artwork-container").innerHTML = "";
+    // Clear the body or another container element
+    document.querySelector("#artwork-container").innerHTML = "";
 
-  data.data.forEach((artwork) => {
-    const imageUrl = `${iiifUrl}/${artwork.image_id}/full/843,1000/0/default.jpg`;
+    data.data.forEach((artwork) => {
+      const imageUrl = `${iiifUrl}/${artwork.image_id}/full/843,1000/0/default.jpg`;
 
-    // Create a new element for the hero section
-    const heroElement = document.createElement("section");
-    heroElement.className = "hero";
-    heroElement.innerHTML = `<img src="${imageUrl}">`;
+      // Create a new element for the hero section
+      const heroElement = document.createElement("section");
+      heroElement.className = "hero";
+      heroElement.innerHTML = `<img src="${imageUrl}">`;
 
-    // Append the hero section to the body
-    document.querySelector("body").appendChild(heroElement);
+      // Append the hero section to the body
+      document.querySelector("body").appendChild(heroElement);
 
-    // Create a new element for the box
-    const boxElement = document.createElement("div");
-    boxElement.className = "box";
-    boxElement.innerHTML = `
-          <img src="${imageUrl}">
-          <h2>${artwork.title}</h2>
-          <p>${artwork.dimensions}</p>
-          <p>${artwork.artwork_type_title}</p>
-          <p>${artwork.style_title}</p>`;
+      // Create a new element for the box
+      const boxElement = document.createElement("div");
+      boxElement.className = "box";
+      boxElement.innerHTML = `
+            <img src="${imageUrl}">
+            <h2>${artwork.title}</h2>
+            <p>${artwork.dimensions}</p>
+            <p>${artwork.artwork_type_title}</p>`;
 
-    // Append the box to the #artwork-container
-    document.querySelector("#artwork-container").appendChild(boxElement);
-  });
+      // Append the box to the #artwork-container
+      document.querySelector("#artwork-container").appendChild(boxElement);
+    });
+  } catch (error) {
+    console.error(error);
+    // If a 403 or 404 error occurs, call the fetchData function again to load another set of artworks
+    if (error.status === 403 || error.status === 404) {
+      fetchData();
+    }
+  }
 };
 
 fetchData();
