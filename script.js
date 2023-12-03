b365590eb940869bd81b4858db9939dd4a127d4f
hugosandsjo <hugosandsjo@gmail.com>
2023-12-03 08:49:10 +0100

    added styling

--- script.js
+++ script.js
@@ -1,60 +1,48 @@
 // const fetchData = async () => {
 //   const res = await fetch("https://api.artic.edu/api/v1/artworks");
 //   const data = await res.json();
-//   const iiifUrl = data.config.iiif_url; // Get the base IIIF Image API endpoint
+//   const iiifUrl = data.config.iiif_url;
 
 //   data.data.forEach((artwork) => {
-//     if (artwork.image_id) {
-//       // Check if image_id is available
-//       const imageUrl = `${iiifUrl}/${artwork.image_id}/full/843,1000/0/default.jpg`; // Construct the image URL
-//       document.querySelector("body").innerHTML += `<div class="box">
-//             <img src="${imageUrl}">
-//                     <h1>${artwork.title}</h1>
-//                     <p>${artwork.dimensions}</p>
-//             </div>`;
-//     }
+//     const imageUrl = `${iiifUrl}/${artwork.image_id}/full/843,1000/0/default.jpg`;
+//     document.querySelector("body").innerHTML += `
+//     <section class="hero">
+//     <img src="${imageUrl}"></section>`;
+//     document.querySelector("#artwork-container").innerHTML += `<div class="box">
+//           <img src="${imageUrl}">
+//               <h2>${artwork.title}</h2>
+//               <p>${artwork.dimensions}</p>
+//               <p>${artwork.artwork_type_title}</p>
+//               <p>${artwork.style_title}</p>
+//           </div>`;
 //   });
-
-//   console.log(data);
 // };
 
 // fetchData();
 
 const fetchData = async () => {
-  const res = await fetch("https://api.artic.edu/api/v1/artworks");
+  // Generate a random number between 1 and 100
+  const limit = Math.floor(Math.random() * 100) + 1;
+
+  const res = await fetch(
+    `https://api.artic.edu/api/v1/artworks?limit=${limit}`,
+  );
   const data = await res.json();
-  const iiifUrl = data.config.iiif_url; // Get the base IIIF Image API endpoint
+  const iiifUrl = data.config.iiif_url;
 
   data.data.forEach((artwork) => {
-    if (artwork.image_id) {
-      // Check if image_id is available
-      const imageUrl = `${iiifUrl}/${artwork.image_id}/full/843,1000/0/default.jpg`; // Construct the image URL
-      fetch(imageUrl)
-        .then((res) => {
-          if (!res.ok) {
-            throw new Error(`HTTP error! status: ${res.status}`);
-          }
-          return res.blob();
-        })
-        .then((blob) => {
-          let objectURL = URL.createObjectURL(blob);
-          document.querySelector(
-            "#artwork-container",
-          ).innerHTML += `<div class="box">
-                  <img src="${objectURL}">
-                          <h1>${artwork.title}</h1>
-                          <p>${artwork.dimensions}</p>
-                  </div>`;
-        })
-        .catch((e) =>
-          console.log(
-            "There has been a problem with your fetch operation: " + e.message,
-          ),
-        );
-    }
+    const imageUrl = `${iiifUrl}/${artwork.image_id}/full/843,1000/0/default.jpg`;
+    document.querySelector("body").innerHTML += `
+    <section class="hero">
+    <img src="${imageUrl}"></section>`;
+    document.querySelector("#artwork-container").innerHTML += `<div class="box">
+          <img src="${imageUrl}">
+              <h2>${artwork.title}</h2>
+              <p>${artwork.dimensions}</p>
+              <p>${artwork.artwork_type_title}</p>
+              <p>${artwork.style_title}</p>
+          </div>`;
   });
-
-  console.log(data);
 };
 
 fetchData();
